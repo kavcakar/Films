10aeb05edccb0eea1c8731e21b1016d808d10d65
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-14 01:33:23 -0400

    cretae model

--- movies/movieapp/models.py
+++ movies/movieapp/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class 
+class Contain(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.CharField(max_length=55)
+    genre = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    writer = models.CharField(max_length=255)
+    language = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

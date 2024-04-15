6ab97658f84b89f392c8241fbdf9af5a06ef3f8f
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-15 11:42:22 -0400

    view

---
+++ movies/movieapp/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from movieapp.models import Contain
+from movieapp.api.serializers import ContainSerializer
+
+@api_view(['GET'])
+def contain_list_create_api_view(request):
+
+    if request.method == 'GET':
+        

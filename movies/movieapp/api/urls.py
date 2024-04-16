3e33847f7991940eb1a53a4830e56aaf1e03b3fc
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-16 02:36:03 -0400

    put delete

--- movies/movieapp/api/urls.py
+++ movies/movieapp/api/urls.py
@@ -3,4 +3,5 @@ from movieapp.api import views as api_views
 
 urlpatterns = [
     path('contains/', api_views.contain_list_create_api_view, name='contain-list'),
+    path('contains/<int:pk>', api_views.contain_detail_api_view, name='contain-detail')
 ]

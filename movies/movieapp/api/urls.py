2126cd99d6d44a99e31e4f4b1aad13ef7889c4c7
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-17 14:52:46 -0400

    class continue urls

--- movies/movieapp/api/urls.py
+++ movies/movieapp/api/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from movieapp.api import views as api_views
 
 urlpatterns = [
-    path('contains/', api_views.contain_list_create_api_view, name='contain-list'),
-    path('contains/<int:pk>', api_views.contain_detail_api_view, name='contain-detail')
+    path('contains/', api_views.ContainListCreateAPIView.as_view(), name='contain-list'),
+    path('contains/<int:pk>', api_views.ContainDetailAPIView.as_view(), name='contain-detail')
 ]
+
+
+                  ### FUNCTION BASED Views###
+# urlpatterns = [
+#     path('contains/', api_views.contain_list_create_api_view, name='contain-list'),
+#     path('contains/<int:pk>', api_views.contain_detail_api_view, name='contain-detail')
+# ]

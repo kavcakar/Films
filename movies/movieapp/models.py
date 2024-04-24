d0443fc070e7300cd9e03a0c7457600244c6c94e
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-24 12:43:35 -0400

    last

--- movies/movieapp/models.py
+++ movies/movieapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+
 class Contain(models.Model):
     title = models.CharField(max_length=255)
     year = models.CharField(max_length=55)
